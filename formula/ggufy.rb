4b46c0670bb599c6fd571d06fab36fdbe3c4042d
nbiish <10566304+nbiish@users.noreply.github.com>
2025-11-15 13:40:58 -0500

    brew: pin to v0.1.1 source tarball with sha256

--- formula/ggufy.rb
+++ formula/ggufy.rb
@@ -1,8 +1,9 @@
 class Ggufy < Formula
   desc "Unified GGUF wrapper for llama.cpp and Ollama"
   homepage "https://github.com/nbiish/ggufy"
-  url "https://github.com/nbiish/ggufy.git", :using => :git, :tag => "v0.1.0"
-  version "0.1.0"
+  url "https://github.com/nbiish/ggufy/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "b33fb644c7a1c6aa8a7be1a8b836d5a7c29f8b67ae1464e9e550405f05c7e236"
+  version "0.1.1"
 
   depends_on "rust" => :build
 
